d2b0c343c5bc60fecd24db2d4936ae24e1a5cd98
lolPirate <debamalyadawn21@gmail.com>
2020-06-05 13:03:57 +0530

    Misc style fix

--- scripts/covid_data_analysis.py
+++ scripts/covid_data_analysis.py
@@ -5,9 +5,9 @@ from pprint import pprint
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+plt.style.use('bmh')
 
-COUNTRY = 'italy'
+COUNTRY = 'india'
 STATUS = ['confirmed', 'recovered', 'deaths']
 API = 'https://api.covid19api.com/dayone/country/{}/status/{}/live'
 DATA_FOLDER_PATH = os.path.normpath(r'./data/')
@@ -80,16 +80,19 @@ month_fmt = mdates.DateFormatter('%M')
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), dpi=120)
 ax1.plot(dates_confirmed, cases_confirmed, color='k', label='Confirmed')
 ax1.plot(dates_recovered, cases_recovered, color='g', label='Recovered')
-ax1.plot(dates_active, cases_active,'--', color='b', label='Active')
+ax1.plot(dates_active, cases_active, '--', color='b', label='Active')
 ax1.plot(dates_deaths, cases_deaths, color='r', label='Deaths')
 ax1.xaxis.set_major_locator(months)
 ax1.set_title(f'Cumulative')
 ax1.legend(loc='best')
 
-ax2.plot(dates_confirmed, cases_confirmed_daily, color='k', label='Confirmed')
-ax2.plot(dates_recovered, cases_recovered_daily, color='g', label='Recovered')
-#ax2.plot(dates_active, cases_active_daily, color='b', label='Active', marker='.')
-ax2.plot(dates_deaths, cases_deaths_daily, color='r', label='Deaths')
+ax2.plot(dates_confirmed, cases_confirmed_daily,
+         color='k', label='Reported', alpha=0.6)
+ax2.plot(dates_recovered, cases_recovered_daily,
+         color='g', label='Recovered', alpha=0.6)
+#ax2.plot(dates_active, cases_active_daily, color='b', label='Active', marker='.', alpha=0.6)
+ax2.plot(dates_deaths, cases_deaths_daily,
+         color='r', label='Deaths', alpha=0.6)
 ax2.xaxis.set_major_locator(months)
 ax2.set_title(f'Daily Totals')
 ax2.legend(loc='best')
@@ -97,6 +100,6 @@ ax2.legend(loc='best')
 
 date = dates_confirmed[-1].astype('datetime64[D]')
 plt.suptitle(f'Covid-19 Data Analysis for {COUNTRY.upper()} as of {date}')
-plt.savefig(os.path.join(PLOTS_FOLDER_PATH, f'covid-19-analysis-{COUNTRY}-latest.jpg'), dpi=720, bbox_inches='tight')
+plt.savefig(os.path.join(PLOTS_FOLDER_PATH,
+                         f'covid-19-analysis-{COUNTRY}-latest.jpg'), dpi=720, bbox_inches='tight')
 plt.show()
-
